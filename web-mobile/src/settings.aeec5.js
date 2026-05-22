eab988a4c8d91641d1e319d696496e8a8a1620e5
wangZquan <1138257147@qq.com>
2026-05-22 15:30:30 +0800

    ads

---
+++ web-mobile/src/settings.aeec5.js
@@ -0,0 +1,1 @@
+window._CCSettings={platform:"web-mobile",groupList:["default"],collisionMatrix:[[true]],hasResourcesBundle:true,hasStartSceneBundle:false,remoteBundles:[],subpackages:[],launchScene:"db://assets/Scene/gameHome.fire",orientation:"",jsList:[],bundleVers:{internal:"d0832",resources:"75c79",main:"c0fe2"}};
